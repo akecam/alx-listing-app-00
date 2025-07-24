0009c031eae709a2b89cc0b9d655716ebf9c6d98
Donald Umeh <donaldumeh54@gmail.com>
2025-07-24 02:10:13 +0100

    added constant

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
-}
+import React from "react";
+
+const Home = () => {
+  return <div className="">Home</div>;
+};
+
+export default Home;
